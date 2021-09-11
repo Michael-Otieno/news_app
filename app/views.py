bf82209a26d9452780f9544bdb104476b140aaf3
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-11 12:23:08 +0300

    add h1 to index.html

--- app/views.py
+++ app/views.py
@@ -1,1 +1,12 @@
 # Where we will create all our view functions.
+from flask import render_template
+from app import app
+
+#views
+@app.route('/')
+def index():
+    '''
+    view root page function that returns the index page and its data
+    '''
+    title = 'Popular News'
+    return render_template('index.html', title = title)
