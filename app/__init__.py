71763ecd44f714d34c6b671a3cdf1b45ad6530dd
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-12 00:57:02 +0300

    add bootstrap to the environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # Where we will initialize our application.
 from flask import Flask
 from app.config import DevConfig
-
+from flask_bootstrap import Bootstrap
 
 #initializing application
 app = Flask(__name__, instance_relative_config=True)
@@ -9,6 +9,9 @@ app = Flask(__name__, instance_relative_config=True)
 #setting up configurations
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#Initializing flask extensions
+bootstrap = Bootstrap(app)
 
 
 from app import views
